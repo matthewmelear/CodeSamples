6a0289b7a48c785779cab2e30574bf5c22de7f32
matthewmelear@gmail.com <matthewmelear@gmail.com>
2014-09-01 16:21:29 -0700

    ATM apps

---
+++ AtmConsoleApp/bin/Validator.java
@@ -0,0 +1,33 @@
+
+public class Validator {
+
+	public boolean isValidTransactionTypeDecision(String transactionTypeDecision) {
+		return transactionTypeDecision.equals("1") ||
+				transactionTypeDecision.equals("2") ||
+				transactionTypeDecision.equals("3");
+	}
+
+	public boolean isValidTransactionDesiredInput(String transactionDesired) {
+		return transactionDesired.equals("1") ||
+			   transactionDesired.equals("2");
+	}
+
+	public boolean isValidDollarAmount(String amount) {
+		
+		boolean hasValidCents = true;
+		boolean isValidNumber = true;
+		if (amount.contains(".")){
+			hasValidCents = amount.substring(amount.indexOf(".") + 1).length() == 2;
+		} 
+		
+		try {
+		  double postCastAmount = Double.parseDouble(amount);
+		  isValidNumber = postCastAmount > 0 ? true : false;
+		} catch (Exception e){
+			isValidNumber = false;
+		}
+		
+		return hasValidCents && isValidNumber;
+	}
+	
+}
